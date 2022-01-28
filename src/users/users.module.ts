281f31a21307dc3168e331b2ffb0a48c9f55d2e4
JeongHoon Park <kmclo@naver.com>
2022-01-28 23:54:41 +0900

    :fire: 필요 없는 코드 삭제

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cart } from 'src/orders/entities/cart.entity';
-import { Comment } from 'src/posts/entities/comment.entity';
 import { User } from './entities/user.entity';
 import { UserResolver } from './users.resolver';
 import { UserService } from './users.service';
